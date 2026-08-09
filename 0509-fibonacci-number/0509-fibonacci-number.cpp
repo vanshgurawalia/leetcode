cce09fbf73b3e6dd51d3748386ebd2a9cbdce125
VANSH <114214080+vanshgurawalia@users.noreply.github.com>
2026-08-09 15:07:56 +0530

    Time: 3 ms (64.76%), Space: 7.7 MB (65.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
+    int arr[31];
     int fib(int n) {
         if (n==0 || n==1){
             return n;
         }
-        return fib(n-1) + fib(n-2);
+        if(arr[n]==0){
+            arr[n] = fib(n-1) + fib(n-2);
+        }
+        return arr[n];
     }
 };
