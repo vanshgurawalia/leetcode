3c55221872ac48707035fadadb2e02587801254b
VANSH <114214080+vanshgurawalia@users.noreply.github.com>
2026-09-20 20:31:29 +0530

    Time: 0 ms (100%), Space: 22.7 MB (53.58%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -7,30 +7,18 @@ public:
 
         for(int r=0 ; r<9 ; r++){
             for(int c=0 ; c<9 ; c++){
+                if(board[r][c] == '.') continue;
 
-                if(board[r][c] == '.'){
-                    continue;
-                }
+                int val = board[r][c] - '1';
 
-                int val=board[r][c]-'1';
-                
-                if(rows[r][val]==1){
-                    return false;
-                }
-
+                if(rows[r][val] == 1) return false;
                 rows[r][val] = 1;
 
-                if(cols[c][val]==1){
-                    return false;
-                }
-
+                if(cols[c][val] == 1) return false;
                 cols[c][val] = 1;
 
                 int boxidx = 3*(r/3)+(c/3);
-                if(boxes[boxidx][val]==1){
-                    return false;
-                }
-
+                if(boxes[boxidx][val] == 1) return false;
                 boxes[boxidx][val] = 1;
             }
         }
