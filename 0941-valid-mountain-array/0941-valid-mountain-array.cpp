88258a0ded904ff1e17d49f8622021160e319cf0
VANSH <114214080+vanshgurawalia@users.noreply.github.com>
2026-09-24 18:18:28 +0530

    Time: 21 ms (42.35%), Space: 26.3 MB (56.98%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -3,6 +3,7 @@ public:
     bool validMountainArray(vector<int>& arr) {
         int i = 0;
         int n = arr.size();
+        if(n<3) return false;
 
         while((i+1 < n) && arr[i]<arr[i+1]) i++;
 
